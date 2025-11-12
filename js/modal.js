5ff10981a2fe44ba463eb94cfd5b248b6a9d85b9
aleksi2105 <aleksi2105@yandex.ru>
2025-11-12 16:12:07 +0300

    Функции инкапсулированы

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,26 @@
-const modalBtn = document.querySelector('.modal__button')
-const getBtn = document.querySelector('.course__button')
-const modal = document.querySelector('.modal') 
+const modal = () => {
+  const modalBtn = document.querySelector('.modal__button')
+  const getBtn = document.querySelector('.course__button')
+  const modal = document.querySelector('.modal')
 
-modalBtn.addEventListener('click', () => {
-  modal.style.display = 'flex'  
-})
+  modalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex'
+  })
 
-getBtn.addEventListener('click', () => {
-  modal.style.display = 'flex'  
-})
+  getBtn.addEventListener('click', () => {
+    modal.style.display = 'flex'
+  })
 
-modal.addEventListener('click', (event) =>{
-  const modalContent = event.target.closest('.modal__inner')
-  
-  if (!modalContent) {
-    modal.style.display = '' 
-      
-  } 
-  
-})
+  modal.addEventListener('click', (event) => {
+    const modalContent = event.target.closest('.modal__inner')
+
+    if (!modalContent) {
+      modal.style.display = ''
+
+    }
+
+  })
+
+}
+
+modal()
